9270fa7f3fdd42238c8a2bd2fe7aa1870ff4b396
Rakanitschu <42042309+Rakanitschu@users.noreply.github.com>
2025-03-14 23:45:29 +0100

    Create actions.js

---
+++ data/actions.js
@@ -0,0 +1,39 @@
+export const actionsData = [
+    {
+      title: 'Switch to LED Bulbs',
+      summary: 'Use energy-efficient LED bulbs to reduce electricity usage.',
+      imageUrl: 'https://images.unsplash.com/photo-1596877440946-331b64c24c7c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxlZCUyMGJ1bGJ8ZW58MHx8MHx8fDI%3D',
+      detail:
+        'LED bulbs use up to 85% less energy than traditional bulbs, significantly reducing electricity bills and environmental impact.',
+      benefit:
+        'Saves up to $75 per year in energy bills and reduces carbon footprint.',
+    },
+    {
+      title: 'Smart Power Strips',
+      summary: 'Utilize smart power strips to cut off power when not in use.',
+      imageUrl: 'https://images.unsplash.com/photo-1601467450590-8c3d11cde2fd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FmZXR5JTIwcGx1Z3xlbnwwfHwwfHx8Mg%3D%3D',
+      detail:
+        'Smart power strips prevent phantom energy use by cutting power to electronics when they are turned off.',
+      benefit:
+        'Can save over $100 annually on your energy bill and decrease unnecessary energy waste.',
+    },
+    {
+      title: 'Window Insulation',
+      summary: 'Improve your home’s insulation to save energy.',
+      imageUrl: 'https://images.unsplash.com/photo-1465577512280-1c2d41a79862?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2luZG93fGVufDB8fDB8fHwy',
+      detail:
+        "Proper window insulation can reduce heating and cooling costs by keeping your home's temperature regulated without overusing HVAC systems.",
+      benefit:
+        'Reduces heating and cooling costs by up to 20%, enhancing comfort and saving money.',
+    },
+    {
+      title: 'Green Energy Plan',
+      summary: 'Opt for green energy plans from your utility provider.',
+      imageUrl: 'https://images.unsplash.com/photo-1497440001374-f26997328c1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Z3JlZW4lMjBlbGVjdHJpY2l0eXxlbnwwfHwwfHx8Mg%3D%3D',
+      detail:
+        'Choosing a green energy plan helps transition to renewable energy sources like wind and solar, which are more sustainable.',
+      benefit:
+        'Supports the renewable energy industry and reduces reliance on fossil fuels.',
+    },
+  ];
+  
